4ffe3f1958735d50d0a47c4005daed16680295dc
CheoR <cheo.roman+github@gmail.com>
2021-02-17 10:35:38 -0600

    feat - render employee location

--- scripts/employees/employeeDataProvider.js
+++ scripts/employees/employeeDataProvider.js
@@ -40,17 +40,42 @@
       "name": "accounting"
     }
   }
+
+  Object format with computerId and departmentId expanded
+  http://localhost:8088/employees?_expand=computer&_expand=department&_expand=location
+  {
+    "id": 1,
+    "firstName": "Mouse-A",
+    "lastName": "Surname-A",
+    "computerId": 6,
+    "departmentId": 1,
+    "locationId": 1,
+    "computer": {
+      "id": 6,
+      "model": "Macbook Pro",
+      "year": 2014
+    },
+    "department": {
+      "id": 1,
+      "name": "accounting"
+    },
+    "location": {
+      "id": 1,
+      "location": "Nashville"
+    }
+  }
 */
 
 let _employees = []
-const url = "http://localhost:8088/employees"
-const urlWithExpandComputer = "http://localhost:8088/employees?_expand=computer"
-const urlWithExpandComputerAndDept = "http://localhost:8088/employees?_expand=computer&_expand=department"
+// const url = "http://localhost:8088/employees"
+// const urlExpandComputer = "http://localhost:8088/employees?_expand=computer"
+// const urlExpandComputerDept = "http://localhost:8088/employees?_expand=computer&_expand=department"
+const  urlExpandComputerDeptLoc = "http://localhost:8088/employees?_expand=computer&_expand=department&_expand=location"
 
 export const useEmployees = () => _employees.slice()
 
 export const getEmployees = () => {
- return fetch(urlWithExpandComputerAndDept)
+ return fetch(urlExpandComputerDeptLoc)
   .then(response => response.json())
   .then(employeeList => {
    _employees = employeeList
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,4 @@
-// import { getComputers, useComputers } from "./computers/computerDataProvider.js"
-// import { getEmployees, useEmployees } from "./employees/employeeDataProvider.js"
-import { computerList } from "./computers/computerList.js"
 import { employeeList } from "./employees/employeeList.js"
 
 console.log("Welcome to the main module")
-computerList()
 employeeList()
